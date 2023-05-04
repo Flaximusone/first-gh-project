c62f8525d25a6a4a3a73f0ded941325f376ce282
Flaximusone <131370176+Flaximusone@users.noreply.github.com>
2023-05-04 13:20:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Gibt Popup Meldung aus
+  // alert('Funktion ist gestartet')
+
+// Ausgabe auf der Browserkonsole 
+   //console.log('Ich bin ein Konselnausdruck')
+
+
+
+   
+   function toggleBox(){
+    //Element holen
+   let meineBox = document.querySelector('.versucheinsjs')
+
+    //Die Klasse verändern (toggeln)
+    meineBox.classList.toggle('none')
+}
